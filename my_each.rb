27979369f1db78724b6a4037560a77f943ef1e0f
Rabiah Hogans <hogansrabiah@gmail.com>
2020-03-30 04:21:05 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,6 @@
-def my_each # put argument(s) here
-  # code here
+def my_each (arraay)             # put argument(s) here
+ collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  puts i
+end                                     # code here
 end
